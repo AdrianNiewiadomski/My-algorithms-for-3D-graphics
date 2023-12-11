8b61812738fbb8b3988b53f55bd519158384053e
AdrianNiewiadomski <adrian.niewiadomski.biuro@gmail.com>
2023-12-12 00:19:58 +0100

    fix: static path have been changed.

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 import os
+import pathlib
 
 from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
+PATH = str(pathlib.Path(__file__).parent.resolve())
 
 
 @app.route("/")
@@ -12,14 +14,14 @@ def display_home():
 
 @app.route("/cross_section_view")
 def display_cross_section_view():
-    path = url_for("static", filename='_models')
-    files = os.listdir(path[1:])
+    models_path = PATH + url_for("static", filename='_models')
+    files = os.listdir(models_path)
     return render_template("cross_section_view.html", files=files)
 
 
 @app.route("/model_corrector_view")
 @app.route("/model_corrector_view/<chosen_file>")
-def display_corrector_view(chosen_file: str="text.txt"):
-    path = url_for("static", filename="_incorrect_models")
+def display_corrector_view(chosen_file: str = "text.txt"):
+    path = PATH + url_for("static", filename="_incorrect_models")
     files = os.listdir(path[1:])
     return render_template("model_corrector_view.html", files=files, chosen_file=chosen_file)
